7e06e4aaf6fee46b36b04dbd458801122da9cad2
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 16:30:40 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -8,12 +8,21 @@ const urll = process.env.MONGODB_URLL;
 http.createServer(function (req, res) {
 if (req.url == "/")
 {
-		file = 'index.html';
-		fs.readFile(file, function(err, txt) {
-			if(err) { return console.log(err); }
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(txt);
-		});
-	setTimeout(function(){res.end();}, 2000);
+	file = 'index.html';
+	fs.readFile(file, function(err, txt) {
+		if(err) { return console.log(err); }
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(txt);
+	});
+else if (req.url == "/process")
+{
+	res.writeHead(200, {'Content-Type':'text/html'});
+	console.log("Process the form");
+	pdata = "";
+	req.on('data', data => {
+           pdata += data.toString();
+         });
+	
+setTimeout(function(){res.end();}, 2000);
 }
 }).listen(port);
