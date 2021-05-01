1fb58576a4ddc6570bdd3ca7ded0c759b0896f31
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 13:28:50 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const urll ="mongodb+srv://unellu01:aaa@cluster0.xd9qb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 http.createServer(function (req, res) {
-	file = 'ticker.html';
+	file = 'index.html';
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write("Hello World");
 	res.end();
