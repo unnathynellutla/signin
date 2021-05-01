8be3795172f2dd73b7a45193f7a0f68c5cfdb2b0
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 13:49:27 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
 const { MongoClient } = require("mongodb");
-const urll ="mongodb+srv://unellu01:<password>@cluster0.trnuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 http.createServer(function (req, res) {
 	file = "index.html";
