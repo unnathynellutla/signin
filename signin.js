8572fcdb960a314c36250e8f41c1576bf4ad16fc
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 16:40:08 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,10 +1,3 @@
-var http = require('http');
-var fs = require('fs');
-var qs = require('querystring');
-var port = process.env.PORT || 3000;
-const { MongoClient } = require("mongodb");
-const urll = process.env.MONGODB_URLL;
-
 http.createServer(function (req, res) {
 if (req.url == "/")
 {
@@ -14,6 +7,7 @@ if (req.url == "/")
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(txt);
 	});
+}
 else if (req.url == "/process")
 {
 	res.writeHead(200, {'Content-Type':'text/html'});
@@ -21,8 +15,31 @@ else if (req.url == "/process")
 	pdata = "";
 	req.on('data', data => {
            pdata += data.toString();
-         });
-	
-setTimeout(function(){res.end();}, 2000);
+    });
+	req.on('end', () => {
+	pdata = qs.parse(pdata);
+	var name = String(pdata['fullname']);
+	var email = String(pdata['email']);
+		
+	MongoClient.connect(urll, { useUnifiedTopology: true }, function(err, db) {
+	  if(err) { return console.log(err); }
+
+		var dbo = db.db("users");
+		var collection = dbo.collection('profiles');
+
+		var newData = {"fullname": name, "email": email,foods: []};
+		collection.insertOne(newData, function(err, res) {
+		if(err) { console.log("query err: " + err); return; }
+		console.log("new document inserted");
+		}   );
+
+		console.log("Success!");
+
+	});  
+	});
+
 }
+setTimeout(function(){res.end();}, 2000);
 }).listen(port);
+
+
