e5f4ad729f9b2183a9a22ee21cabdc62c7c230ee
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 17:53:24 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -15,5 +15,36 @@ if (req.url == "/")
 		res.write(txt);
 	});
 }
+else if (req.url == "/process")
+{
+	res.writeHead(200, {'Content-Type':'text/html'});
+	console.log("Process the form");
+	pdata = "";
+	req.on('data', data => {
+           pdata += data.toString();
+    });
+	req.on('end', () => {
+	pdata = qs.parse(pdata);
+	var name = String(pdata['fullname']);
+	var email = String(pdata['email']);
+		
+	MongoClient.connect(urll, { useUnifiedTopology: true }, function(err, db) {
+	  if(err) { return console.log(err); }
+
+		var dbo = db.db("users");
+		var collection = dbo.collection('profiles');
+
+		var newData = {"fullname": name, "email": email};
+		collection.insertOne(newData, function(err, res) {
+		if(err) { console.log("query err: " + err); return; }
+		console.log("new document inserted");
+		}   );
+
+		console.log("Success!");
+
+	});  
+	});
+
+}
 setTimeout(function(){res.end();}, 2000);
 }).listen(port);
