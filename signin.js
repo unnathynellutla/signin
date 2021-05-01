59448d8f32cebd6c5a7ab8f2673557a60c07c884
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 13:50:57 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 3000;
-const { MongoClient } = require("mongodb");
 
 http.createServer(function (req, res) {
 	file = "index.html";
