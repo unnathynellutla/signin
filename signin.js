b2af04c8b7a9ed0b45c877fedaa0ae9ca6bf1189
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-01 14:43:44 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -6,6 +6,9 @@ const { MongoClient } = require('mongodb');
 const urll =process.env.MONGODB_URLL;
 
 http.createServer(function (req, res) {
+res.write("Hello World");
+if (req.url == "/")
+{
 	res.write("Hello World");
 		file = 'index.html';
 		fs.readFile(file, function(err, txt) {
@@ -13,6 +16,7 @@ http.createServer(function (req, res) {
 			res.write("Hello World");
 			res.write(txt);
 		});
+}
 
 	res.end();
 }).listen(port);
